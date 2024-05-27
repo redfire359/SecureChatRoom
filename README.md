3c0bc31c825d2b86807167325dbf43dc30d1c987
redfire359 <redfire359@duck.com>
2024-05-27 14:43:52 -0400

    c

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # SecureChatRoom
 Secure Chat room written in c/c++ 
 https://gist.github.com/aa6my/c8e088b0ee3b24a56d4e0ebc974055a4
+
+
+C++ Client -> sends message to -> python server
--- pythonUnsecure/server-unsecure.py
+++ pythonUnsecure/server-unsecure.py
@@ -1,45 +1,37 @@
-#!/usr/bin/python3
 '''
-Python Server Code 
+Our server will open a socket on port 443, waiting for incoming connections
+it will then accept and print out any messages we receive 
 
 '''
-# stop the __pychache__ folder 
-import sys
-sys.dont_write_bytecode = True
-
-import socketserver
-import socket
-import os
+import socket 
 import threading 
 
 IP = '192.168.56.3'
 port = 443
-num_connections = 4 
+
+def connections(socket_connection):
+    while True:
+        try:
+            message = socket_connection.recv(1024)
+            if message:
+                print("Got message")
+                print(f"Message: {message.decode('utf-8')}")
+        except Exception as e:
+            print(f"Error: {e}")
 
 
-def recv(client_socket):
-    message = client_socket.recv(1024)
+try:
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((IP, port))
+    server_socket.listen(3)
+    print(f"Binded to port {port}")
 
     while True:
-        if message:
-            print(msg.decode('utf-8'))
-        else:
-            client_socket.close()
-            break
-        
+        socket_conn, address = server_socket.accept()
+        print(f"Connection from: {address}")
+        connections(socket_conn)
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((IP, port))
-server_socket.listen(num_connections)
-print('[*] Listening...')
+    server_socket.close()
 
-client_socket, address = server_socket.accept()
-print(f"Connections from: {address}")
-
-t1 = threading.Thread(target=recv, name='t1', args=[client_socket]).start()
-
-while True:
-
-    userInput = input("Send: ")
-    message = userInput.encode('utf-8')
-    client_socket.send(message)
+except Exception as e:
+    print(f"Exception occurred: {e}")
